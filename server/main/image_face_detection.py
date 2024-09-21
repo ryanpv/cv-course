901e8c47729e59bf18136f3f10de00965c57489c
Ryan <ryanpvoong@gmail.com>
2024-09-20 21:59:12 -0400

    looping through face images

--- server/main/image_face_detection.py
+++ server/main/image_face_detection.py
@@ -8,3 +8,18 @@ all_face_locations = face_recognition.face_locations(image_to_detect, model='hog
 # all_face_locations = face_recognition.face_locations(image_to_detect, model='cnn')
 
 print("Number of faces in this photo is: {}".format(len(all_face_locations)))
+
+
+# Looping through the face locations
+for index, current_face_location in enumerate(all_face_locations):
+    top, right, bottom, left = current_face_location
+    print(
+        "Found face {} at top: {}, right: {}, bottom: {}, left: {}".format(
+            index + 1, top, right, bottom, left
+        )
+    )
+    current_face_image = image_to_detect[top:bottom, left:right]
+    cv2.imshow("Face no. " + str(index + 1), current_face_image)
+
+    cv2.waitKey(0)
+    # cv2.destroyAllWindows()
